efe9b8189e7909e5da877a3d37aba71d1b9f7a56
denisvil02 <denisvil02@gmail.com>
2022-02-05 22:37:17 +0200

    H/T

---
+++ Task_2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Dogs:IAnimals
+    {
+        
+        public string Name { get; set; }
+        public string Age { get; set; }
+
+        public static implicit operator Dogs(Cats v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
